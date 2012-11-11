089cf3d88b8fddf0977d2902a29d0b129bc807ac
igaysinskiy <igaysinskiy@reqo.com>
2012-11-11 15:07:14 -0500

    Added automatic creation of index

--- search/src/main/java/com/reqo/ironhold/search/IndexService.java
+++ search/src/main/java/com/reqo/ironhold/search/IndexService.java
@@ -1,12 +1,17 @@
 package com.reqo.ironhold.search;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.elasticsearch.ElasticSearchException;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
@@ -17,15 +22,11 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.FilterBuilders;
-import org.elasticsearch.index.query.FilteredQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
-import org.elasticsearch.search.sort.SortOrder;
 
 import com.reqo.ironhold.storage.model.MailMessage;
+import static junit.framework.Assert.*;
 
 public class IndexService {
 	private static final int RETRY_SLEEP = 10000;
@@ -33,10 +34,10 @@ public class IndexService {
 
 	private static Logger logger = Logger.getLogger(IndexService.class);
 	private Client esClient;
-	private final String client;
+	private final String indexName;
 
-	public IndexService(String client) {
-		this.client = client;
+	public IndexService(String indexName) {
+		this.indexName = indexName;
 		reconnect();
 
 	}
@@ -53,6 +54,33 @@ public class IndexService {
 
 	}
 
+	public void createIndex() throws Exception {
+		CreateIndexResponse response1 = esClient.admin().indices()
+				.prepareCreate(indexName).execute().actionGet();
+		if (!response1.acknowledged()) {
+			throw new Exception("ES Request did not get acknowledged: "
+					+ response1.toString());
+		}
+		String jsonDef = readJsonDefinition();
+		PutMappingResponse response2 = esClient.admin().indices()
+				.preparePutMapping(indexName).setType("message")
+				.setSource(jsonDef).execute().actionGet();
+		if (!response2.acknowledged()) {
+			throw new Exception("ES Request did not get acknowledged: "
+					+ response2.toString());
+		}
+
+	}
+
+	public void dropIndex() throws Exception {
+		DeleteIndexResponse response = esClient.admin().indices()
+				.prepareDelete(indexName).execute().actionGet();
+		if (!response.acknowledged()) {
+			throw new Exception("ES Request did not get acknowledged: "
+					+ response.toString());
+		}
+	}
+
 	public boolean store(MailMessage message) throws JsonGenerationException,
 			JsonMappingException, ElasticSearchException, IOException {
 		if (!exists(message.getMessageId())) {
@@ -61,7 +89,7 @@ public class IndexService {
 			while (!success && retry <= MAX_RETRY_COUNT) {
 				try {
 
-					esClient.prepareIndex(client, "message",
+					esClient.prepareIndex(indexName, "message",
 							message.getMessageId())
 							.setSource(MailMessage.toJSON(message)).execute()
 							.actionGet();
@@ -95,28 +123,61 @@ public class IndexService {
 
 	private boolean exists(String messageId) {
 		GetResponse response = esClient
-				.prepareGet(client, "message", messageId).execute().actionGet();
+				.prepareGet(indexName, "message", messageId).execute()
+				.actionGet();
 		return response.isExists();
 	}
 
-
 	public MessageSearchBuilder getNewBuilder() {
-		return MessageSearchBuilder.newBuilder(esClient.prepareSearch(client));
+		return MessageSearchBuilder.newBuilder(esClient
+				.prepareSearch(indexName));
 	}
-	
+
 	public SearchResponse search(MessageSearchBuilder builder) {
 		return builder.build().execute().actionGet();
 	}
-	
+
 	public long getMatchCount(String search) {
 
-		SearchRequestBuilder builder = esClient.prepareSearch(client);
+		SearchRequestBuilder builder = esClient.prepareSearch(indexName);
 		QueryBuilder qb = QueryBuilders.queryString(search);
 		builder.setQuery(qb);
 		builder.setSearchType(SearchType.COUNT);
 		SearchResponse response = builder.execute().actionGet();
 
 		return response.getHits().getTotalHits();
+	}
+
+	private static String readJsonDefinition() throws Exception {
+		return readFileInClasspath("/estemplate/message.json");
+	}
+
+	/**
+	 * Read a file in classpath and return its content
+	 * 
+	 * @param url
+	 *            File URL Example : /es/twitter/_settings.json
+	 * @return File content or null if file doesn't exist
+	 * @throws Exception
+	 */
+	public static String readFileInClasspath(String url) throws Exception {
+		StringBuffer bufferJSON = new StringBuffer();
+
+		try {
+			InputStream ips = IndexService.class.getResourceAsStream(url);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				bufferJSON.append(line);
+			}
+			br.close();
+		} catch (Exception e) {
+			return null;
+		}
+
+		return bufferJSON.toString();
 	}
 
 }
