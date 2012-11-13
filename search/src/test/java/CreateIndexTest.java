536e34053da2d7747c39524838b4258fd8b79cc2
igaysinskiy <igaysinskiy@reqo.com>
2012-11-13 18:33:55 -0500

    Working facets

--- search/src/test/java/CreateIndexTest.java
+++ search/src/test/java/CreateIndexTest.java
@@ -1,37 +1,29 @@
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import com.reqo.ironhold.search.IndexService;
+import org.junit.*;
 
 
 public class CreateIndexTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void test() throws Exception {
-		IndexService indexService = new IndexService("unittest");
-		indexService.createIndex();
-		indexService.dropIndex();
-	}
+    @Test
+    public void test() throws Exception {
+        IndexService indexService = new IndexService("unittest");
+        indexService.dropIndex();
+    }
 
 }
---
+++ web/src/main/java/com/reqo/ironhold/web/components/SourceView.java
@@ -0,0 +1,56 @@
+package com.reqo.ironhold.web.components;
+
+import com.reqo.ironhold.search.IndexFieldEnum;
+import com.reqo.ironhold.search.IndexService;
+import com.reqo.ironhold.search.IndexUtils;
+import com.reqo.ironhold.storage.IStorageService;
+import com.reqo.ironhold.storage.model.MailMessage;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.search.SearchHit;
+
+@SuppressWarnings("serial")
+public class SourceView extends Panel {
+
+    private final IStorageService storageService;
+    private final IndexService indexService;
+    private final SourceView me;
+    private SearchHitPanel currentHitPanel;
+
+    public SourceView(IStorageService storageService, IndexService indexService) {
+        this.storageService = storageService;
+        this.indexService = indexService;
+        this.setSizeFull();
+        this.me = this;
+    }
+
+    public synchronized void show(SearchHitPanel newHitPanel, SearchHit item, String criteria) throws Exception {
+
+        this.removeAllComponents();
+
+        final Label messageId = new Label("MessageId: " + item.getId());
+        this.addComponent(messageId);
+
+        MailMessage mailMessage = storageService.getMailMessage(item.getId(), true);
+
+        String rawBody = MailMessage.toPrettyJSON(mailMessage).replaceAll("\r?\n", "<br/>");
+
+        final Label body = new Label(rawBody);
+
+        body.setContentMode(Label.CONTENT_RAW);
+        this.addComponent(body);
+
+    }
+
+    private void addPartyLabel(SearchHit item, IndexFieldEnum field) {
+        String value = IndexUtils.getFieldValue(item, field, false);
+        if (!value.equals(StringUtils.EMPTY)) {
+            final Label from = new Label(String.format("%s: %s", field.getLabel(), value));
+            from.setContentMode(Label.CONTENT_XHTML);
+            this.addComponent(from);
+        }
+
+    }
+
+}
