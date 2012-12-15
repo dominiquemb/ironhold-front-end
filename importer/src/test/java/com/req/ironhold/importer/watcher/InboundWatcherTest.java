374e63fe5b04b092f093c1c3f0db596e9fac5e3a
igaysinskiy <igaysinskiy@reqo.com>
2012-12-14 21:25:40 -0500

    Added more tests

--- importer/src/test/java/com/req/ironhold/importer/watcher/InboundWatcherTest.java
+++ importer/src/test/java/com/req/ironhold/importer/watcher/InboundWatcherTest.java
@@ -1,6 +1,8 @@
 package com.req.ironhold.importer.watcher;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -8,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import junit.framework.Assert;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -73,6 +76,63 @@ public class InboundWatcherTest {
 
 		Assert.assertEquals(0, inFolder.getRoot().listFiles().length);
 		Assert.assertEquals(2, queueFolder.getRoot().listFiles().length);
+
+		iw.deActivate();
+		executorService.shutdown();
+
+		while (!executorService.isTerminated()) {
+			executorService.awaitTermination(1000, TimeUnit.MILLISECONDS);
+		}
+
+	}
+	
+	@Test
+	public void testInboundWatcherInvalid() throws Exception {
+		EmailNotification.disableNotification();
+		final InboundWatcher iw = new InboundWatcher(inFolder.getRoot().toString(),
+				queueFolder.getRoot().toString(), quarantineFolder
+				.getRoot().toString(), client);;
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		executorService.execute(new Runnable() {
+
+			@Override
+			public void run() {
+
+				try {
+					iw.start();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+		
+		/* Make sure watcher is fully started */
+		while (!iw.isStarted()) {
+			Thread.sleep(100);
+		}
+		Thread.sleep(100);
+		
+		
+		File pstFile = FileUtils.toFile(InboundWatcherTest.class
+				.getResource(PST_TEST_FILE));
+
+		FileUtils.copyFileToDirectory(pstFile, inFolder.getRoot());
+		
+		File md5File = new File(pstFile.getParent() + File.separator + FilenameUtils.getBaseName(pstFile.toString()) + ".md5");
+		FileWriter fw = new FileWriter(md5File.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("md5" + " "  + pstFile.getName());
+		bw.close();
+		
+		FileUtils.copyFileToDirectory(md5File, inFolder.getRoot());
+
+		Thread.sleep(2000);
+
+		Assert.assertEquals(0, inFolder.getRoot().listFiles().length);
+		Assert.assertEquals(0, queueFolder.getRoot().listFiles().length);
+		Assert.assertEquals(2, quarantineFolder.getRoot().listFiles().length);
 
 		iw.deActivate();
 		executorService.shutdown();
