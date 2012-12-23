9824546df04336ff13a18d22291e7c384ba7fff3
igaysinskiy <igaysinskiy@reqo.com>
2012-12-23 10:30:43 -0500

    Added more tests with image

--- storage/src/test/java/com/reqo/ironhold/storage/EmlLoadTest.java
+++ storage/src/test/java/com/reqo/ironhold/storage/EmlLoadTest.java
@@ -285,23 +285,151 @@ public class EmlLoadTest {
 				mailMessage.getMessageId());
 	}
 	
-	private static byte[] createChecksum(byte[] bytes)
-			throws NoSuchAlgorithmException, IOException {
 
-		InputStream fis = new ByteArrayInputStream(bytes);
+	@Test
+	public void testMimeMessageWithImage() throws Exception {
+		File file = FileUtils.toFile(EmlLoadTest.class
+				.getResource("/testMimeMessageWithImage.eml"));
+		InputStream is = new FileInputStream(file);
+		MimeMessage mimeMessage = new MimeMessage(null, is);
 
-		byte[] buffer = new byte[1024];
-		MessageDigest complete = MessageDigest.getInstance("MD5");
-		int numRead;
-		do {
-			numRead = fis.read(buffer);
-			if (numRead > 0) {
-				complete.update(buffer, 0, numRead);
+		MailMessage mailMessage = new MailMessage(mimeMessage, source);
+
+		Assert.assertEquals(mimeMessage.getSubject(), mailMessage
+				.getImapMailMessage().getSubject());
+		Assert.assertEquals(mimeMessage.getReceivedDate(), mailMessage
+				.getImapMailMessage().getMessageDate());
+
+		Assert.assertEquals(
+				((InternetAddress) mimeMessage.getFrom()[0]).getAddress(),
+				mailMessage.getImapMailMessage().getFrom().getAddress());
+		Assert.assertEquals(
+				((InternetAddress) mimeMessage.getFrom()[0]).getPersonal(),
+				mailMessage.getImapMailMessage().getFrom().getName());
+
+		Assert.assertEquals(mimeMessage.getRecipients(RecipientType.TO).length,
+				mailMessage.getImapMailMessage().getTo().length);
+		if (mimeMessage.getRecipients(RecipientType.CC) != null) {
+
+			Assert.assertEquals(
+					mimeMessage.getRecipients(RecipientType.CC).length,
+					mailMessage.getImapMailMessage().getCc().length);
+
+			for (int i = 0; i < mimeMessage.getRecipients(RecipientType.CC).length; i++) {
+				Assert.assertEquals(((InternetAddress) mimeMessage
+						.getRecipients(RecipientType.CC)[i]).getAddress(),
+						mailMessage.getImapMailMessage().getCc()[i]
+								.getAddress());
+				Assert.assertEquals(((InternetAddress) mimeMessage
+						.getRecipients(RecipientType.CC)[i]).getPersonal(),
+						mailMessage.getImapMailMessage().getCc()[i].getName());
 			}
-		} while (numRead != -1);
-		fis.close();
-		return complete.digest();
+		} else {
+			Assert.assertEquals(0,
+					mailMessage.getImapMailMessage().getCc().length);
+		}
+
+		if (mimeMessage.getRecipients(RecipientType.BCC) != null) {
+
+			Assert.assertEquals(
+					mimeMessage.getRecipients(RecipientType.BCC).length,
+					mailMessage.getImapMailMessage().getBcc().length);
+
+			for (int i = 0; i < mimeMessage.getRecipients(RecipientType.BCC).length; i++) {
+				Assert.assertEquals(((InternetAddress) mimeMessage
+						.getRecipients(RecipientType.BCC)[i]).getAddress(),
+						mailMessage.getImapMailMessage().getBcc()[i]
+								.getAddress());
+				Assert.assertEquals(((InternetAddress) mimeMessage
+						.getRecipients(RecipientType.BCC)[i]).getPersonal(),
+						mailMessage.getImapMailMessage().getBcc()[i].getName());
+			}
+		} else {
+			Assert.assertEquals(0,
+					mailMessage.getImapMailMessage().getBcc().length);
+		}
+
+		List<Header> mimeHeaders = Collections
+				.list(mimeMessage.getAllHeaders());
+		Assert.assertEquals(mimeHeaders.size(), mailMessage
+				.getImapMailMessage().getHeaders().length);
+		for (int i = 0; i < mimeHeaders.size(); i++) {
+			Assert.assertEquals(mimeHeaders.get(i).getName(), mailMessage
+					.getImapMailMessage().getHeaders()[i].getName());
+			Assert.assertEquals(mimeHeaders.get(i).getValue(), mailMessage
+					.getImapMailMessage().getHeaders()[i].getValue());
+		}
+
+		MimeMultipart contents = (MimeMultipart) mimeMessage.getContent();
+
+		Multipart part1 = (Multipart) contents.getBodyPart(0).getContent();
+
+		Assert.assertEquals(part1.getBodyPart(0).getContent().toString(),
+				mailMessage.getImapMailMessage().getBody());
+
+		Assert.assertEquals(part1.getBodyPart(0).getContentType(), mailMessage
+				.getImapMailMessage().getBodyContentType());
+
+		Assert.assertEquals(part1.getBodyPart(1).getContent().toString(),
+				mailMessage.getImapMailMessage().getBodyHTML());
+
+		Assert.assertEquals(part1.getBodyPart(1).getContentType(), mailMessage
+				.getImapMailMessage().getBodyHTMLContentType());
+
+		InputStream part2 = (InputStream) contents.getBodyPart(1).getContent();
+		Assert.assertEquals(contents.getBodyPart(1).getFileName(),
+				mailMessage.getAttachments()[0].getFileName());
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		// 8176 is the block size used internally and should give the
+		// best
+		// performance
+
+		byte[] buf = new byte[4096];
+		int bytesRead;
+		while((bytesRead = part2.read(buf))!=-1) {
+		    out.write(buf, 0, bytesRead);
+		}
+		out.close();
+
+		Assert.assertEquals(Base64.encodeBase64String(out.toByteArray()),
+				mailMessage.getAttachments()[0].getBody());
+
+		File attachment = FileUtils.toFile(EmlLoadTest.class.getResource("/"
+				+ mailMessage.getAttachments()[0].getFileName()));
+
+		String md5fromFile = getMD5Checksum(attachment);
+		OutputStream fos = new FileOutputStream(tempFolder.getRoot()
+				+ File.separator
+				+ mailMessage.getAttachments()[0].getFileName());
+		
+		InputStream inputStream = new ByteArrayInputStream(Base64.decodeBase64(mailMessage.getAttachments()[0].getBody().getBytes()));
+		
+		int read = 0;
+		int total = read;
+		byte[] bytes = new byte[1024];
+	 
+		while ((read = inputStream.read(bytes)) != -1) {
+			
+			total += read;
+			fos.write(bytes, 0, read);
+		}
+		inputStream.close();
+		fos.flush();
+		fos.close();
+		
+		
+		String md5fromAttachment = getMD5Checksum(new File(tempFolder.getRoot()
+				+ File.separator
+				+ mailMessage.getAttachments()[0].getFileName()));
+
+		Assert.assertEquals(md5fromFile, md5fromAttachment);
+
+		Assert.assertEquals(mimeMessage.getMessageID(),
+				mailMessage.getMessageId());
 	}
+	
+	
 
 	private static byte[] createChecksum(File file)
 			throws NoSuchAlgorithmException, IOException {
@@ -332,16 +460,6 @@ public class EmlLoadTest {
 		return result;
 	}
 
-	// see this How-to for a faster way to convert
-	// a byte array to a HEX string
-	public static String getMD5Checksum(byte[] bytes)
-			throws NoSuchAlgorithmException, IOException {
-		byte[] b = createChecksum(bytes);
-		String result = "";
-		for (int i = 0; i < b.length; i++) {
-			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
-		}
-		return result;
-	}
+	
 
 }
