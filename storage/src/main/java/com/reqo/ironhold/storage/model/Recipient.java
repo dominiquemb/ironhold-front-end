3d929f49a59709f11643b559f137b7a4568903bf
igaysinskiy <igaysinskiy@reqo.com>
2013-02-03 10:56:30 -0500

    Added more name formatting

--- storage/src/main/java/com/reqo/ironhold/storage/model/Recipient.java
+++ storage/src/main/java/com/reqo/ironhold/storage/model/Recipient.java
@@ -4,59 +4,111 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.text.WordUtils;
 
 public class Recipient {
-    private String name;
-    private String address;
+	private String name;
+	private String address;
 
-    public Recipient() {
+	public Recipient() {
 
-    }
+	}
 
-    public Recipient(String name, String address) {
-        this.name = name == null ? StringUtils.EMPTY : name;
-        this.address = address;
-    }
+	public Recipient(String name, String address) {
+		this.name = name == null ? StringUtils.EMPTY : name;
+		this.address = address;
+	}
 
 	public String getName() {
-        return name;
-    }
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+	@Override
+	public boolean equals(Object rhs) {
+		return EqualsBuilder.reflectionEquals(this, rhs);
 
-    @Override
-    public boolean equals(Object rhs) {
-        return EqualsBuilder.reflectionEquals(this, rhs);
+	}
 
-    }
-
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
 
 	public static Recipient normalize(Recipient recipient) {
-		if (recipient.getName()!=null && recipient.getName().trim().length() > 0) {
+		Recipient result = Recipient.updateNameAndAddress(recipient);
+		return Recipient.formatName(result);
+	}
+
+	private static Recipient formatName(Recipient result) {
+		String name = result.getName().trim();
+
+		// non breaking space
+
+		name = StringUtils.removeStart(name, Character.toString((char) 160));
+
+		while (name.contains("  ")) {
+			name = StringUtils.replace(name, "  ", " ");
+		}
+
+		if (name.contains("@")) {
+			name = name.substring(0, name.indexOf("@"));
+		}
+		if (name.contains("(")) {
+			name = name.substring(0, name.indexOf("("));
+		}
+		if (name.contains("<")) {
+			name = name.substring(0, name.indexOf("<"));
+		}
+		if (name.contains("/")) {
+			name = name.substring(0, name.indexOf("/"));
+		}
+		if (name.contains("[")) {
+			name = name.substring(0, name.indexOf("["));
+		}
+
+		name = name.toLowerCase();
+
+		name = WordUtils.capitalize(name, ' ', '.', '_', '*', ',', '-', '\'');
+		name = name.trim();
+		name = name.replaceAll("^[.,\"'(</]+", "");
+		name = name.replaceAll("[.,\"'(</]+$", "");
+		name = name.trim();
+
+		if (name.length() == 0 || name.matches("^[\\?*\\s*]*$")) {
+			name = "unknown";
+		}
+		/*
+		 * if (!result.getName().equals(name)) {
+		 * System.out.println(String.format( "%s\t%s", result.getName(), name,
+		 * (int) name.charAt(0))); }
+		 */
+		return new Recipient(name, result.getAddress());
+	}
+
+	private static Recipient updateNameAndAddress(Recipient recipient) {
+		if (recipient.getName() != null
+				&& recipient.getName().trim().length() > 0) {
 			return recipient;
 		} else {
 			if (recipient.getAddress() == null) {
-				return new Recipient("unknown","unknown");
+				return new Recipient("unknown", "unknown");
 			}
 			String address = recipient.getAddress().trim();
 			if (address.contains("@")) {
@@ -64,22 +116,22 @@ public class Recipient {
 				if (name.trim().length() > 0) {
 					return new Recipient(name, address);
 				} else {
-					return new Recipient(address, address);	
+					return new Recipient(address, address);
 				}
-				
+
 			} else {
 				return new Recipient(address, address);
 			}
 		}
 	}
-	
+
 	public static Recipient[] normalize(Recipient[] recipients) {
 		Recipient[] results = new Recipient[recipients.length];
-		
-		for (int i =0 ; i< recipients.length; i++) {
+
+		for (int i = 0; i < recipients.length; i++) {
 			results[i] = Recipient.normalize(recipients[i]);
 		}
-		
+
 		return results;
 	}
 }
--- storage/src/test/java/com/reqo/ironhold/storage/model/RecipientTest.java
+++ storage/src/test/java/com/reqo/ironhold/storage/model/RecipientTest.java
@@ -1,8 +1,16 @@
 package com.reqo.ironhold.storage.model;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import junit.framework.Assert;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 public class RecipientTest {
@@ -11,23 +19,23 @@ public class RecipientTest {
 	public void testNormalizeNoWork() {
 		Recipient original = new Recipient("name", "address");
 		Recipient result = Recipient.normalize(original);
-		Assert.assertEquals("name", result.getName());
+		Assert.assertEquals("Name", result.getName());
 		Assert.assertEquals("address", result.getAddress());
 	}
-	
+
 	@Test
 	public void testNormalizeEmptyNameNoAtSign() {
 		Recipient original = new Recipient("", "address");
 		Recipient result = Recipient.normalize(original);
-		Assert.assertEquals("address", result.getName());
+		Assert.assertEquals("Address", result.getName());
 		Assert.assertEquals("address", result.getAddress());
 	}
-	
+
 	@Test
 	public void testNormalizeEmptyNameWithAtSign() {
 		Recipient original = new Recipient("", "address@domain.com");
 		Recipient result = Recipient.normalize(original);
-		Assert.assertEquals("address", result.getName());
+		Assert.assertEquals("Address", result.getName());
 		Assert.assertEquals("address@domain.com", result.getAddress());
 	}
 
@@ -35,7 +43,7 @@ public class RecipientTest {
 	public void testNormalizeEmptyNameWithNoNameAndAtSign() {
 		Recipient original = new Recipient("", "@domain.com");
 		Recipient result = Recipient.normalize(original);
-		Assert.assertEquals("@domain.com", result.getName());
+		Assert.assertEquals("unknown", result.getName());
 		Assert.assertEquals("@domain.com", result.getAddress());
 	}
 
@@ -45,8 +53,58 @@ public class RecipientTest {
 		Recipient[] originals = new Recipient[1];
 		originals[0] = original;
 		Recipient[] result = Recipient.normalize(originals);
-		Assert.assertEquals("@domain.com", result[0].getName());
+		Assert.assertEquals("unknown", result[0].getName());
 		Assert.assertEquals("@domain.com", result[0].getAddress());
+	}
+
+	@Test
+	public void mytest() {
+		String name = "?? ??";
+		
+		Assert.assertTrue(name.matches("^[\\?*\\s*]*$"));
+	}
+
+	@Test
+	public void testGLTNormalizeFile() throws IOException {
+		File file = FileUtils.toFile(RecipientTest.class
+				.getResource("/gltnames.sorted"));
+		List<String> lines = FileUtils.readLines(file);
+		Set<String> cleanLines = new HashSet<String>();
+		for (String line : lines) {
+			cleanLines.add(Recipient.normalize(new Recipient(line, line))
+					.getName());
+		}
+		Assert.assertEquals(lines.size(), 74704);
+		Assert.assertEquals(cleanLines.size(), 59489);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testVBMNormalizeFile() throws IOException {
+		File file = FileUtils.toFile(RecipientTest.class
+				.getResource("/vbmnames.sorted"));
+		List<String> lines = FileUtils.readLines(file);
+		Set<String> cleanLines = new HashSet<String>();
+		for (String line : lines) {
+			cleanLines.add(Recipient.normalize(new Recipient(line, line))
+					.getName());
+		}
+		Assert.assertEquals(lines.size(), 125265);
+		Assert.assertEquals(cleanLines.size(), 94136);
+	}
+
+	@Test
+	public void testTWFNormalizeFile() throws IOException {
+		File file = FileUtils.toFile(RecipientTest.class
+				.getResource("/twfnames.sorted"));
+		List<String> lines = FileUtils.readLines(file);
+		Set<String> cleanLines = new HashSet<String>();
+		for (String line : lines) {
+			cleanLines.add(Recipient.normalize(new Recipient(line, line))
+					.getName());
+		}
+		Assert.assertEquals(lines.size(), 21468);
+		Assert.assertEquals(cleanLines.size(), 18351);
 	}
 
 }
