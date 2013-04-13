d385b2c3b41aedb218407d87be95b8f80ff4efdf
igaysinskiy <igaysinskiy@reqo.com>
2013-04-13 18:14:50 -0400

    Working search permissions

---
+++ storage/src/main/java/com/reqo/ironhold/storage/model/user/RoleEnum.java
@@ -0,0 +1,20 @@
+package com.reqo.ironhold.storage.model.user;
+
+/**
+ * User: ilya
+ * Date: 4/12/13
+ * Time: 12:34 PM
+ */
+public enum RoleEnum {
+    SUPER_USER(Integer.MAX_VALUE), CAN_LOGIN(1), NONE(0), CAN_SEARCH(2);
+
+    private final int value;
+
+    RoleEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
