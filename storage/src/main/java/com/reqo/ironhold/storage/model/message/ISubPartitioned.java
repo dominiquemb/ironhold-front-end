abe2a7e223602626db8197d0c7eb17cfcfa366f4
igaysinskiy <igaysinskiy@reqo.com>
2013-04-14 10:35:52 -0400

    Added message subPartitioning

---
+++ storage/src/main/java/com/reqo/ironhold/storage/model/message/ISubPartitioned.java
@@ -0,0 +1,10 @@
+package com.reqo.ironhold.storage.model.message;
+
+/**
+ * User: ilya
+ * Date: 4/14/13
+ * Time: 9:41 AM
+ */
+public interface ISubPartitioned {
+    String getSubPartition();
+}
