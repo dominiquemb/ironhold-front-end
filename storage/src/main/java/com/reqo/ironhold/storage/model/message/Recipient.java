35a4b8a2cdc221c890819762e249e9ffda4dc786
igaysinskiy <igaysinskiy@reqo.com>
2013-06-22 14:11:52 -0400

    UI uplift, reader changes

--- storage/src/main/java/com/reqo/ironhold/storage/model/message/Recipient.java
+++ storage/src/main/java/com/reqo/ironhold/storage/model/message/Recipient.java
@@ -8,147 +8,150 @@ import org.apache.commons.lang3.text.WordUtils;
 import org.apache.log4j.Logger;
 
 public class Recipient {
-	private static Logger logger = Logger.getLogger(MimeMailMessage.class);
+    private static Logger logger = Logger.getLogger(MimeMailMessage.class);
 
-	private String name;
-	private String address;
-	private String domain;
+    private String name;
+    private String address;
+    private String domain;
 
-	public Recipient() {
+    public Recipient() {
 
-	}
+    }
 
-	public Recipient(String name, String address) {
-		this.name = name == null ? StringUtils.EMPTY : name;
-		this.address = address;
-		this.domain = address != null && address.contains("@")
-				&& address.lastIndexOf('@') < address.length() ? address
-				.substring(address.lastIndexOf('@') + 1) : null;
-	}
+    public Recipient(String name, String address) {
+        this.name = name == null ? StringUtils.EMPTY : name;
+        this.address = address;
+        this.domain = address != null && address.contains("@")
+                && address.lastIndexOf('@') < address.length() ? address
+                .substring(address.lastIndexOf('@') + 1) : null;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public String getDomain() {
-		return domain;
-	}
+    public String getDomain() {
+        return domain;
+    }
 
-	public void setDomain(String domain) {
-		this.domain = domain;
-	}
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this);
-	}
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
 
-	@Override
-	public boolean equals(Object rhs) {
-		return EqualsBuilder.reflectionEquals(this, rhs);
+    @Override
+    public boolean equals(Object rhs) {
+        return EqualsBuilder.reflectionEquals(this, rhs);
 
-	}
+    }
 
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
 
-	public static Recipient normalize(Recipient recipient) {
-		Recipient result = Recipient.updateNameAndAddress(recipient);
-		return Recipient.formatName(result);
-	}
+    public static Recipient normalize(Recipient recipient) {
+        Recipient result = Recipient.updateNameAndAddress(recipient);
+        recipient.setAddress(recipient.getAddress().trim().toLowerCase());
+        recipient.setDomain(recipient.getDomain().trim().toLowerCase());
 
-	private static Recipient formatName(Recipient result) {
-		String name = result.getName().trim();
+        return Recipient.formatName(result);
+    }
 
-		// non breaking space
+    private static Recipient formatName(Recipient result) {
+        String name = result.getName().trim();
 
-		name = StringUtils.removeStart(name, Character.toString((char) 160));
+        // non breaking space
 
-		while (name.contains("  ")) {
-			name = StringUtils.replace(name, "  ", " ");
-		}
+        name = StringUtils.removeStart(name, Character.toString((char) 160));
 
-		if (name.contains("@")) {
-			name = name.substring(0, name.indexOf("@"));
-		}
-		if (name.contains("(")) {
-			name = name.substring(0, name.indexOf("("));
-		}
-		if (name.contains("<")) {
-			name = name.substring(0, name.indexOf("<"));
-		}
-		if (name.contains("/")) {
-			name = name.substring(0, name.indexOf("/"));
-		}
-		if (name.contains("[")) {
-			name = name.substring(0, name.indexOf("["));
-		}
+        while (name.contains("  ")) {
+            name = StringUtils.replace(name, "  ", " ");
+        }
 
-		name = name.toLowerCase();
+        if (name.contains("@")) {
+            name = name.substring(0, name.indexOf("@"));
+        }
+        if (name.contains("(")) {
+            name = name.substring(0, name.indexOf("("));
+        }
+        if (name.contains("<")) {
+            name = name.substring(0, name.indexOf("<"));
+        }
+        if (name.contains("/")) {
+            name = name.substring(0, name.indexOf("/"));
+        }
+        if (name.contains("[")) {
+            name = name.substring(0, name.indexOf("["));
+        }
 
-		name = WordUtils.capitalize(name, ' ', '.', '_', '*', ',', '-', '\'');
-		name = name.trim();
-		name = name.replaceAll("^[.,\"'(</]+", "");
-		name = name.replaceAll("[.,\"'(</]+$", "");
-		name = name.trim();
+        name = name.toLowerCase();
 
-		if (name.length() == 0 || name.matches("^[\\?*\\s*]*$")
-				|| !name.contains(" ")) {
-			name = "unknown";
-		}
+        name = WordUtils.capitalize(name, ' ', '.', '_', '*', ',', '-', '\'');
+        name = name.trim();
+        name = name.replaceAll("^[.,\"'(</]+", "");
+        name = name.replaceAll("[.,\"'(</]+$", "");
+        name = name.trim();
 
-		if (!result.getName().equals(name)) {
-			logger.debug(String.format("%s\t%s", result.getName(), name,
-					(int) name.charAt(0)));
-		}
+        if (name.length() == 0 || name.matches("^[\\?*\\s*]*$")
+                || !name.contains(" ")) {
+            name = "unknown";
+        }
 
-		return new Recipient(name, result.getAddress());
-	}
+        if (!result.getName().equals(name)) {
+            logger.debug(String.format("%s\t%s", result.getName(), name,
+                    (int) name.charAt(0)));
+        }
 
-	private static Recipient updateNameAndAddress(Recipient recipient) {
-		if (recipient.getName() != null
-				&& recipient.getName().trim().length() > 0) {
-			return recipient;
-		} else {
-			if (recipient.getAddress() == null) {
-				return new Recipient("unknown", "unknown");
-			}
-			String address = recipient.getAddress().trim();
-			if (address.contains("@")) {
-				String name = address.substring(0, address.indexOf("@"));
-				if (name.trim().length() > 0) {
-					return new Recipient(name, address);
-				} else {
-					return new Recipient(address, address);
-				}
+        return new Recipient(name, result.getAddress());
+    }
 
-			} else {
-				return new Recipient(address, address);
-			}
-		}
-	}
+    private static Recipient updateNameAndAddress(Recipient recipient) {
+        if (recipient.getName() != null
+                && recipient.getName().trim().length() > 0) {
+            return recipient;
+        } else {
+            if (recipient.getAddress() == null) {
+                return new Recipient("unknown", "unknown");
+            }
+            String address = recipient.getAddress().trim();
+            if (address.contains("@")) {
+                String name = address.substring(0, address.indexOf("@"));
+                if (name.trim().length() > 0) {
+                    return new Recipient(name, address);
+                } else {
+                    return new Recipient(address, address);
+                }
 
-	public static Recipient[] normalize(Recipient[] recipients) {
-		Recipient[] results = new Recipient[recipients.length];
+            } else {
+                return new Recipient(address, address);
+            }
+        }
+    }
 
-		for (int i = 0; i < recipients.length; i++) {
-			results[i] = Recipient.normalize(recipients[i]);
-		}
+    public static Recipient[] normalize(Recipient[] recipients) {
+        Recipient[] results = new Recipient[recipients.length];
 
-		return results;
-	}
+        for (int i = 0; i < recipients.length; i++) {
+            results[i] = Recipient.normalize(recipients[i]);
+        }
+
+        return results;
+    }
 }
