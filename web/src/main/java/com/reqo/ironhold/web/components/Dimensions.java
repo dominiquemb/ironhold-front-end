fd5707d665041f9d814ff1affd084be172012d72
igaysinskiy <igaysinskiy@reqo.com>
2013-06-27 06:56:53 -0400

    UI uplift

---
+++ web/src/main/java/com/reqo/ironhold/web/components/Dimensions.java
@@ -0,0 +1,12 @@
+package com.reqo.ironhold.web.components;
+
+/**
+ * User: ilya
+ * Date: 6/26/13
+ * Time: 6:30 PM
+ */
+public class Dimensions {
+    public static final String LEFT = "200px";
+    public static final String MIDDLE = "620px";
+
+}
