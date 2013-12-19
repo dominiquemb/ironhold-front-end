9757af0febd3cfa006f68037b897df47b33d1d6e
Dominique Moreno-Baltierra <d@dominique.cc>
2013-12-19 04:18:04 -0500

    Adding reset password steps, removing duplicate files

--- web-ui/src/main/webapp/app/js/controllers/LoginController.js
+++ web-ui/src/main/webapp/app/js/controllers/LoginController.js
@@ -12,6 +12,11 @@ ironholdApp.controller('LoginController', function ($http, $resource, $window, $
 	return $scope.currentlyVisible === elemName;
     }
 
+    $scope.verifyResetCode = function(code) {
+	// This is a placeholder for now
+	$scope.makeVisible('forgot-password-step-3');
+    }
+
     $scope.isLoggedIn = function() {
 	return $scope.loggedIn;
     }
