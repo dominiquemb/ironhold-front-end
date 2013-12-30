0099003a69932ab465b2512457c9ccaf490e86f7
igaysinskiy <igaysinskiy@reqo.com>
2013-12-30 13:33:34 -0500

    Fixed compilation error

--- web-domain/src/main/java/com/reqo/ironhold/web/domain/RoleEnum.java
+++ web-domain/src/main/java/com/reqo/ironhold/web/domain/RoleEnum.java
@@ -1,13 +1,12 @@
 package com.reqo.ironhold.web.domain;
 
-import org.springframework.security.access.ConfigAttribute;
 
 /**
  * User: ilya
  * Date: 4/12/13
  * Time: 12:34 PM
  */
-public enum RoleEnum implements ConfigAttribute {
+public enum RoleEnum {
     SUPER_USER(Integer.MAX_VALUE), CAN_LOGIN(1), NONE(0), CAN_SEARCH(2), CAN_MANAGE_USERS(4), CAN_SEARCH_ALL(8), CAN_VIEW_AUDIT(16), PST_UPLOAD(32);
 
     private final int value;
@@ -20,7 +19,6 @@ public enum RoleEnum implements ConfigAttribute {
         return value;
     }
 
-    @Override
     public String getAttribute() {
         return "ROLE_" + this.name();
     }
