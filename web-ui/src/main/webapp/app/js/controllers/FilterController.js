5d2755e501c91507da906a62fabae03f2400dc0f
dm <dm@reqo.com>
2014-01-24 21:20:43 -0500

    Filter Controller

---
+++ web-ui/src/main/webapp/app/js/controllers/FilterController.js
@@ -0,0 +1,41 @@
+'use strict';
+
+ironholdApp.controller('FilterController', function ($http, $resource, $window, $rootScope, $scope, $location, $timeout, Restangular, searchResultsService, $state, logInService, messagesService) {
+    logInService.confirmLoggedIn($state);
+
+    $scope.selectedFilters = [];
+    $scope.filters = [];
+    $scope.showFilters = false;
+
+    $scope.unselectAllFilters = function() {
+        angular.forEach($scope.selectedFilters, function(filter) {
+            filter.selected = false;
+        });
+        $scope.selectedFilters = [];
+    }
+
+    $scope.enableFilter = function(filter, filterGroupCode) {
+	if (filter.selected) {
+	        $scope.selectedFilters.push(filter);
+        } else {
+            $scope.selectedFilters.remove(filter);
+        }
+
+	$scope.$emit('filterToggled', filter);
+    }
+
+    $scope.disableFilter = function(filter) {
+	$scope.$emit('toggleFacet', filter);	
+    }
+
+    $rootScope.$on('facetToggled', function(evt, facet) {
+	$scope.enableFilter(facet);
+    });
+
+    $rootScope.$on('filters', function(evt, filterList) {
+	$scope.filters = filterList;
+	if (filterList.length > 0) {
+		$scope.showFilters = true;
+	}
+    });
+});
