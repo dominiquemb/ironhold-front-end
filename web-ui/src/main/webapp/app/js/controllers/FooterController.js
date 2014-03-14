c1509e61db5eb5cabd74627a43de7a4d17884143
dm <dm@reqo.com>
2014-03-14 19:59:37 +0100

    Second batch of footer updates

--- web-ui/src/main/webapp/app/js/controllers/FooterController.js
+++ web-ui/src/main/webapp/app/js/controllers/FooterController.js
@@ -6,18 +6,33 @@ ironholdApp.controller('FooterController', function ($http, $resource, $window, 
     $scope.searchMatches;
     $scope.selectedFacets;
     $scope.searchTime;
+    $scope.showFooterSearchStats = false;
+    $scope.showAfterFilter = false;
 
     $rootScope.$on('totalResultsChange', function(evt, result) {
 	$scope.totalMessages = result;
     });
 
+    $rootScope.$on('reset', function() {
+	$scope.showFooterSearchStats = false;
+	$scope.searchMatches = 0;
+	$scope.selectedFacets = [];
+	$scope.searchTime = 0;
+	$scope.showAfterFilter = false;
+    });
+
     $rootScope.$on('updateFooter', function(evt, results) {
 	var newFilters = (results.selectedFacets === $scope.selectedFacets) ? false : true;
+	
+	$scope.showFooterSearchStats = true;
+
 	if ($scope.searchMatches > 0 && newFilters) {
 		$scope.afterFilter = results.searchMatches;
+		$scope.showAfterFilter = true;
 	}
 	else {
 	        $scope.searchMatches = results.searchMatches;
+		$scope.showAfterFilter = false;
 	}
         $scope.searchTime = results.searchTime;
     });
