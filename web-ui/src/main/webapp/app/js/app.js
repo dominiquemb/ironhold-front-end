e9195175ab6489573f5c8830a83cfca2be51f2a5
dm <dm@reqo.com>
2014-04-13 21:33:41 +0200

    Facet panel truncation

--- web-ui/src/main/webapp/app/js/app.js
+++ web-ui/src/main/webapp/app/js/app.js
@@ -307,6 +307,27 @@ ironholdApp.filter('to_trusted', ['$sce', function($sce){
         };
 }]);
 
+ironholdApp.filter('truncate', function () {
+	return function (text, length, end) {
+	    if (isNaN(length)) {
+		length = 10;
+	    }
+
+	    if (end === undefined) {
+		end = "...";
+	    }
+
+	    if (text.length <= length || text.length - end.length <= length) {
+		return text;
+	    }
+
+	    else {
+		return String(text).substring(0, length-end.length) + end;
+	    }
+
+	};
+});
+
 Date.prototype.getDayName = function() {
 	var d = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 	return d[this.getDay()];
